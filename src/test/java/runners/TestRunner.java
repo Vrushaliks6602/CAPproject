763de052a0a4ed6edf52ffb86cfc53fdd4e933d2
Vrushali Kiran Sonawane <90602138+Vrushaliks6602@users.noreply.github.com>
2025-03-20 09:00:52 +0530

    Update TestRunner.java

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,7 +6,9 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
     features = "src/test/resources/features/urbanladder.feature",
     glue = "stepDefinitions",
-    plugin = {"pretty", "html:target/cucumber-reports"}
+    //plugin = {"pretty", "html:target/cucumber-reports"},
+    plugin = {"pretty", "json:target/cucumber-reports-json/Cucumber.json"} // Generate JSON report
+
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
